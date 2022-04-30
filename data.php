061caee1968ffdb39df16d3e99fba54a131eed1c
GinilabDev <80915429+GinilabDev@users.noreply.github.com>
2022-05-01 02:45:55 +0600

    create, delete, update

--- data.php
+++ data.php
@@ -4,7 +4,16 @@ $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 if(!$conn){
     throw new Exception('Connection failed');
 }else{
-    echo 'connected';
+    //echo mysqli_query($conn, "INSERT INTO tasks (task, date) VALUES ('Tomorrow will go to dhamaich', '2022-05-01')");
+    mysqli_query($conn, "DELETE FROM tasks WHERE id = 2");
+    $result = mysqli_query($conn, "SELECT * FROM tasks");
+    while($data = mysqli_fetch_assoc($result)){
+        echo "<pre>";
+        print_r($data);
+        echo "</pre>";
+    }
+
+    mysqli_close($conn);
 }
 
 // $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASWORD,DB_NAME);
