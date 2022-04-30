d73331ca0cf8bd1d57858452323de4e58dacbc54
GinilabDev <80915429+GinilabDev@users.noreply.github.com>
2022-05-01 02:46:23 +0600

    Update data.php

--- data.php
+++ data.php
@@ -5,13 +5,13 @@ if(!$conn){
     throw new Exception('Connection failed');
 }else{
     //echo mysqli_query($conn, "INSERT INTO tasks (task, date) VALUES ('Tomorrow will go to dhamaich', '2022-05-01')");
-    mysqli_query($conn, "DELETE FROM tasks WHERE id = 2");
-    $result = mysqli_query($conn, "SELECT * FROM tasks");
-    while($data = mysqli_fetch_assoc($result)){
-        echo "<pre>";
-        print_r($data);
-        echo "</pre>";
-    }
+    // mysqli_query($conn, "DELETE FROM tasks WHERE id = 2");
+    // $result = mysqli_query($conn, "SELECT * FROM tasks");
+    // while($data = mysqli_fetch_assoc($result)){
+    //     echo "<pre>";
+    //     print_r($data);
+    //     echo "</pre>";
+    // }
 
     mysqli_close($conn);
 }
